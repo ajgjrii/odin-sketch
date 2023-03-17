2202811d4ea79898b80ddccc9864353d8fdc7556
Andrew Gaytan <andrewutmail@gmail.com>
2023-03-17 10:20:24 -0500

    added slider for dimensions control

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // const grid is needed for DOM methods
 const grid = document.querySelector('.sketch');
 
-
-
-
 function createPixels (row, column){
     n=1;
 
@@ -20,15 +17,14 @@ function createPixels (row, column){
         }
     } 
 
-createPixels(8,8);
+createPixels(15,15);
 
 // Array needed for SelectorAll
 const filling = Array.from(document.querySelectorAll('.col'));
-
-
-// forEach is needed since filling is an array
 filling.forEach(filling => filling.addEventListener('mouseover', fill));
 
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', clear)
 
 
 // function adds a class called "filler" to any div.col elements
@@ -36,8 +32,8 @@ function fill() {
     this.classList.add('filler');
 }
 
-const reset = document.querySelector('#reset');
-reset.addEventListener('click', () => {
+// function is used for resetting filled boxes
+ function clear() {
     const empty = Array.from(document.querySelectorAll('.filler'));
     empty.forEach(empty => empty.classList.remove('filler'));
-})
+ }
