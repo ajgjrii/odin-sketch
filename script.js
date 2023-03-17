1b726553d27e47896544cc0f3409f7faa1180d9b
Andrew Gaytan <andrewutmail@gmail.com>
2023-03-17 09:27:44 -0500

    Completed js for the filler. Added style change to css

--- script.js
+++ script.js
@@ -5,22 +5,30 @@ const grid = document.querySelector('.sketch');
 
 
 function createPixels (row, column){
+    n=1;
+
     for (let i = 1; i <= row; ++i) {
         const makeRow = document.createElement('div');
         makeRow.classList.add("row");
         grid.appendChild(makeRow);
         for (let j = 1; j <= column; ++j){
+
             const makeCol = document.createElement('div');
             makeCol.classList.add("col");
             makeRow.appendChild(makeCol);
+            }
         }
     } 
+
+createPixels(8,8);
+
+
+const pixel = Array.from(document.querySelectorAll('.col'));
+pixel.forEach(pixel => pixel.addEventListener('mouseover', fill));
+
+
+
+function fill() {
+    //var fillMe = document.getElementsByClassName('col');
+    this.classList.add('filler');
 }
-
-createPixels(12, 12);
-
-// START HERE
-const fill = document.querySelector('.row');
-fill.addEventListener('mouseover', ()=> {
-    makeRow.setAttribute('style', 'background-color: blue');
-})
