8b0578378bea005c52579a90d6a18f367b9dd309
Andrew Gaytan <andrewutmail@gmail.com>
2023-03-18 14:15:17 -0500

    gave sizing buttons ability to create pixel dimensions; clears child nodes when resizing

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// const grid is needed for DOM methods
-const grid = document.querySelector('.sketch');
-
-
+// function creates the "pixels" in div.sketch
 function createPixels (row, column){
-    n=1;
-
     for (let i = 1; i <= row; ++i) {
         const makeRow = document.createElement('div');
         makeRow.classList.add("row");
@@ -14,19 +9,9 @@ function createPixels (row, column){
             const makeCol = document.createElement('div');
             makeCol.classList.add("col");
             makeRow.appendChild(makeCol);
-            }
         }
-    } 
-
-createPixels(10, 10);
-
-// Array needed for SelectorAll
-const filling = Array.from(document.querySelectorAll('.col'));
-filling.forEach(filling => filling.addEventListener('mouseover', fill));
-
-const reset = document.querySelector('#reset');
-reset.addEventListener('click', clear)
-
+    }
+} 
 
 // function adds a class called "filler" to any div.col elements
 function fill() {
@@ -34,10 +19,51 @@ function fill() {
 }
 
 // function is used for resetting filled boxes
- function clear() {
+function clear() {
     const empty = Array.from(document.querySelectorAll('.filler'));
     empty.forEach(empty => empty.classList.remove('filler'));
  }
+
+// This function will remove all child nodes in DOM (parent is div.sketch)
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+ }
+
+const remove = document.querySelector('#xxx');
+remove.addEventListener('click', ()=> {
+    const sketch = document.querySelector('.sketch');
+    removeAllChildNodes(sketch);
+})
+
+
+
+// const grid is needed for DOM methods
+const grid = document.querySelector('.sketch');
+
+const ten = document.querySelector('#ten');
+ten.addEventListener('click', () => {
+    const sketch = document.querySelector('.sketch');
+    removeAllChildNodes(sketch);
+    createPixels(10, 10);
+    // These two lines create the ability to fill
+    const filling = Array.from(document.querySelectorAll('.col'));
+    filling.forEach(filling => filling.addEventListener('mouseover', fill));
+});
+
+const fifteen = document.querySelector('#fifteen');
+fifteen.addEventListener('click', () => {
+    const sketch = document.querySelector('.sketch');
+    removeAllChildNodes(sketch);
+    createPixels(15,15);
+    // These two lines create the ability to fill
+    const filling = Array.from(document.querySelectorAll('.col'));
+    filling.forEach(filling => filling.addEventListener('mouseover', fill));
+});
+
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', clear)
 
 
  
